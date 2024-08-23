b5ef4ebd151c8deecf65c470a1521f05628b0792
DaliLando <landolsimohamedali@gmail.com>
2024-08-23 11:36:22 +0100

    final update

--- handlers/user.js
+++ handlers/user.js
@@ -1,11 +1,13 @@
 const User = require('../models/userSchema');
 
+// Existing functions...
+
 exports.getProfile = async (req, res) => {
   try {
     const user = await User.findById(req.user.id);
-    return res.status(200).JSON({msg:"user found",user});
+    return res.status(200).json({ msg: "user found", user });
   } catch (error) {
-    res.status(500).json({msg:'Server error occured'});
+    res.status(500).json({ msg: 'Server error occurred' });
   }
 };
 
@@ -17,18 +19,38 @@ exports.updateProfile = async (req, res) => {
       { firstName, lastName },
       { new: true }
     );
-    return res.status(200).json({msg:"Profile Updated",user});
+    return res.status(200).json({ msg: "Profile Updated", user });
   } catch (error) {
-    res.status(500).JSON({msg:'Server error occured'});
+    res.status(500).json({ msg: 'Server error occurred' });
   }
 };
 
-exports.getAllUsers = async (req,res)=>{
+exports.getAllUsers = async (req, res) => {
   try {
-    const userList = await User.find();
-    return res.status(200).json({msg:"Users fetched successfully",userList});
+    const userList = await User.find({role:"user"});
+    return res.status(200).json({ msg: "Users fetched successfully", userList });
   } catch (error) {
-    res.status(500).JSON({msg:'Server error occured'});
+    res.status(500).json({ msg: 'Server error occurred' });
+  }
+};
 
+// New functions to block and unblock users
+exports.blockUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    await User.findByIdAndUpdate(userId, { isBlocked: true });
+    return res.status(200).json({ msg: "User blocked successfully" });
+  } catch (error) {
+    res.status(500).json({ msg: 'Server error occurred' });
+  }
+};
+
+exports.unblockUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    await User.findByIdAndUpdate(userId, { isBlocked: false });
+    return res.status(200).json({ msg: "User unblocked successfully" });
+  } catch (error) {
+    res.status(500).json({ msg: 'Server error occurred' });
   }
 };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const { getProfile, updateProfile, getAllUsers } = require('../handlers/user');
+const { getProfile, updateProfile, getAllUsers, blockUser, unblockUser } = require('../handlers/user');
 const { isAuth } = require('../middlewares/isAuth');
-const {isAdmin} = require ("../middlewares/isAdmin");
-
+const { isAdmin } = require('../middlewares/isAdmin');
 
 let userRouter = express.Router();
 
 userRouter.get('/profile', isAuth, getProfile);
 userRouter.put('/profile', isAuth, updateProfile);
-userRouter.get('/manage',isAdmin,getAllUsers)
+userRouter.get('/manage', isAdmin, getAllUsers);
+userRouter.patch('/block/:id', isAdmin, blockUser);
+userRouter.patch('/unblock/:id', isAdmin, unblockUser);
 
 module.exports = userRouter;
